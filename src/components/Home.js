38731033c97daa925fa63ee9f24e95a5b9343bec
william lee <will.lee.npc@gmail.com>
2023-12-10 01:29:06 -0500

    complete lab

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,17 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+const firebrickColor = "#B22222"
+
+const divStyle = {
+  backgroundColor: firebrickColor,
+}
+
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return <div id="home">
+    <h1 style={divStyle}>{name} is a Web Developer from {city}</h1>
+  </div>;
 }
 
 export default Home;
